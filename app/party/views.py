768023f8db2d783f0fdb8566a882838e987a63b3
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-05 16:57:25 +0300

    [Finishes #163707089] fixed failing tests

--- app/party/views.py
+++ app/party/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from app.models import PartyModel
 from app.party import party
 
@@ -13,5 +13,28 @@ def get_parties():
     else:
         return make_response(jsonify({
             'status':'OK',
-            'parties': parties_db
+            'parties': PartyModel.parties_db
         }), 200)
+
+@party.route('/addparty', methods=['POST'])
+def add_party():
+    data = request.get_json()
+    name = data['name']
+    hqAddress = data['hqAddress']
+    logoUrl = data['logoUrl']
+    id = len(PartyModel.parties_db) + 1
+
+    new_party = {
+        'id' : id,
+        'name' : name,
+        'hqAddress' : hqAddress,
+        'logoUrl' : logoUrl 
+    }
+    
+    PartyModel.parties_db.append(new_party)
+
+    return make_response(jsonify({
+        'Status' : 'OK',
+        'Message' : 'New Party added',
+        'Party' : new_party['name']
+    }), 201)
