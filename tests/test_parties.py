08a1b3168c2307d8bf975be90656f86396dc960c
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-05 12:52:57 +0300

    [Feature #163707256] add party model, views and test files

---
+++ tests/test_parties.py
@@ -0,0 +1,22 @@
+import unittest
+import json
+from app.models import PartyModel
+
+class TestParty(unittest.TestCase):
+    def setup():
+        pass
+
+    def test_get_parties():
+        pass
+    
+    def test_get_a_party():
+        pass
+
+    def test_edit_a_party():
+        pass
+
+    def test_delete_a_party():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
