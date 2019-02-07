74d98db3afe622c96331de3c678816d590dc8446
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-07 10:07:49 +0300

    [Finishes #163708180] Add route for adding a new office

--- app/office/views.py
+++ app/office/views.py
@@ -15,3 +15,23 @@ def get_all_offices():
             'status' : 200,
             'offices' : OfficeModel.offices_db
         }), 200)
+
+@office.route('/addoffices', methods=['POST'])
+def add_office():
+    data = request.get_json()
+    name = data['name']
+    type = data['type']
+    id = len(OfficeModel.offices_db) + 1
+
+    new_office = {
+        'id' : id,
+        'name' : name,
+        'type' : type
+    }
+    
+    OfficeModel.offices_db.append(new_office)
+
+    return make_response(jsonify({
+        'Status' : 201,
+        'Office' : new_office
+    }), 201)
