2fc8a917ca3f964644f026b8bef0fabc87cfb1c1
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-08 08:38:25 +0300

    [Starts #163812352] add comments for run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@
 
 from app import create_app
 
+#create an instance of app called app.
 app = create_app('development')
 
+#run app when flask run is entered on terminal.
 if __name__ == '__main__':
     app.run()
