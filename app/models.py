e0dd34d74d97b043212419f3c5510c5b81864f42
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-12 19:08:17 +0300

    [Chore #163903197] remove all functions from party view to models

--- app/models.py
+++ app/models.py
@@ -2,12 +2,80 @@ class PartyModel:
     '''an instance of the data structures that are required to to strore party data'''
     parties_db = []
 
-    def __init__(self, name, hqAddress, logoUrl):
-        self.name = name
-        self.hqAddress = hqAddress
-        self.logoUrl = logoUrl
-        self.id = len(parties_db) + 1
+    def create_party(data):
 
+        name = data['name'].strip()
+        hqAddress = data['hqAddress'].strip()
+        logoUrl = data['logoUrl'].strip()
+        id = len(PartyModel.parties_db) + 1
+
+        if not name:
+            return [400 ,'name cannot be empty']
+        elif not hqAddress:
+            return [400 ,'hqAddress cannot be empty']
+        elif not logoUrl:
+            return [400, 'logoUrl cannot be empty']
+        
+        for party in PartyModel.parties_db:
+            if party['name'] == name:
+                return [400, 'A party with that name already exists']
+        
+        #creates a new party filled with all required data.
+        new_party = {
+            'id' : id,
+            'name' : name,
+            'hqAddress' : hqAddress,
+            'logoUrl' : logoUrl 
+        }
+    
+        #adds new_party to parties_db
+        PartyModel.parties_db.append(new_party)
+
+        return [201, new_party]
+
+    def get_all_parties():
+
+        if len(PartyModel.parties_db) <= 0:
+            return [404, 'No Parties to be shown']
+        else:
+            return [200, PartyModel.parties_db]
+
+    def get_specific_party(party_id):
+
+        for party in PartyModel.parties_db:
+            if party['id'] == party_id:
+                return [200, party]
+
+        return [ 404, 'Party does not exist']
+    
+    def edit_a_party(party_id, data):
+        name = data['name'].strip()
+
+        if not name:
+            return [404, 'name cannot be empty']
+
+
+        for party in PartyModel.parties_db:
+            if party['name'] == name:
+                return [400, 'name already exists']
+
+        for party in PartyModel.parties_db:
+            if party['id'] == party_id:
+                party['name'] = name
+                return [200, party]
+
+        return [404, 'party not found']
+
+    def delete_specific_party(party_id):
+        for party in PartyModel.parties_db:
+            if party['id'] == party_id:
+                index = party_id - 1
+                PartyModel.parties_db.pop(index)
+                return [200, 'Party has been succefully deleted']
+        
+        return [404, 'party not found']
+
+        
 class OfficeModel:
     '''an instance of the data structure that is required to strore office data'''
     offices_db = []
@@ -56,6 +124,13 @@ class OfficeModel:
 
     def edit_specific_office(office_id, data):
         name = data['name'].strip()
+
+        if not name:
+            return [404, 'name cannot be empty']
+
+        for office in OfficeModel.offices_db:
+            if office['name'] == name:
+                return [400, 'name already exists']
 
         for office in OfficeModel.offices_db:
             if office['id'] == office_id:
--- app/party/views.py
+++ app/party/views.py
@@ -6,135 +6,78 @@ from app.party import party
 
 @party.route('/parties', methods=['GET'])
 def get_parties():
-    #checks to ensure that there are existing parties to get
-    if len(PartyModel.parties_db) <= 0:
+    response = PartyModel.get_all_parties()
+
+    if response[0] == 200:
         return make_response(jsonify({
-            'status': 404,
-            'message': 'No parties to show'
-        }), 404)
+            'status' : response[0],
+            'data' : response[1]
+        }), response[0])
     else:
         return make_response(jsonify({
-            'status': 200,
-            'parties': PartyModel.parties_db
-        }), 200)
+            'status' : response[0],
+            'error' : response[1]
+        }), response[0])
 
 @party.route('/parties', methods=['POST'])
 def add_party():
     data = request.get_json()
-    name = data['name'].strip()
-    hqAddress = data['hqAddress'].strip()
-    logoUrl = data['logoUrl'].strip()
-    id = len(PartyModel.parties_db) + 1
 
-    #Validations to check that correct data is being added to parties_db.
-    if not name:
+    response = PartyModel.create_party(data)
+
+    if response[0] == 201:
         return make_response(jsonify({
-            'status': 400,
-            'error': 'Name cannot be empty'
-        }), 400)
-    elif not hqAddress:
+            'status' : response[0],
+            'data' : response[1]
+        }), response[0])
+    else:
         return make_response(jsonify({
-            'status': 400,
-            'error': 'HQ address cannot be empty'
-        }), 400)
-    elif not logoUrl:
-        return make_response(jsonify({
-            'status': 400,
-            'error': 'Logo URLcannot be empty'
-        }), 400)
-
-    #checks lengh of name.
-    if len(name) > 20:
-        return make_response(jsonify({
-            'status': 400,
-            'error': 'Name cannot be longer than 20 characters'
-        }), 400)
-
-    #Checks for existing party name
-    for party in PartyModel.parties_db:
-        if name == party['name']:
-            return make_response(jsonify({
-                'status': 400,
-                'error': 'A Party with that name already exists'
-            }), 400) 
-
-    #creates a new party filled with all required data.
-    new_party = {
-        'id' : id,
-        'name' : name,
-        'hqAddress' : hqAddress,
-        'logoUrl' : logoUrl 
-    }
-    
-    #adds new_party to parties_db
-    PartyModel.parties_db.append(new_party)
-
-    return make_response(jsonify({
-        'Status' : 200,
-        'Message' : 'New Party added',
-        'Party' : new_party['name']
-    }), 201)
+            'status' :response[0], 
+            'error' : response[1]
+        }), response[0])
 
 @party.route('/parties/<party_id>', methods=['GET'])
 def get_a_party(party_id):
-    #checks if party is existing in partes_db.
-    for party in PartyModel.parties_db:
-        if party['id'] == int(party_id):
-            return make_response(jsonify({
-                'status' : 200,
-                'data' : party
-            }), 200)
+    response = PartyModel.get_specific_party(int(party_id))
 
-    return make_response(jsonify({
-        'status': 404,
-        'error': 'Party does not exist'
-    }), 404)
+    if response[0] == 200:
+        return make_response(jsonify({
+            'status' : response[0],
+            'data' : response[1]
+        }), response[0])
+    else:
+        return make_response(jsonify({
+            'status' : response[0],
+            'error' : response[1]
+        }), response[0])
 
 @party.route('/parties/<party_id>/name', methods=['PATCH'])
 def edit_a_party(party_id):
     data = request.get_json()
-    name = data['name']
+    response = PartyModel.edit_a_party(int(party_id), data)
 
-    #validations to ensure only correct data is used for editing
-    if not name:
+    if response[0] == 200:
         return make_response(jsonify({
-            'status': 400,
-            'error': 'Name cannot be empty'
-        }), 400)
-    elif len(name) > 20:
+            'status' : response[0],
+            'data' : response[1]
+        }), response[0])
+    else:
         return make_response(jsonify({
-            'status': 400,
-            'error': 'Name cannot be longer than 20 characters'
-        }), 400)
-
-    for party in PartyModel.parties_db:
-        if party['id'] == int(party_id):
-            party['name'] = name
-            return make_response(jsonify({
-                'status' : 200,
-                'data' : party
-                }), 200)
-
-    return make_response(jsonify({
-        'status' : 404,
-        'error' : 'Party Not Found'
-    }), 404)
+            'status' : response[0],
+            'error' : response[1]
+        }), response[0])
 
 @party.route('/parties/<party_id>', methods=['DELETE'])
 def delete_a_party(party_id):
+    response = PartyModel.delete_specific_party(int(party_id))
 
-    #the for loop loops through every party and finds the one matching id in the list 
-    for party in PartyModel.parties_db:
-        if party['id'] == int(party_id):
-            index = int(party_id) - 1
-            #the macthing party is removed from the list witj pop() functions.
-            PartyModel.parties_db.pop(index)
-            return make_response(jsonify({
-                'status' : 200,
-                'message' : 'Party has been deleted successfuly'
-            }), 200)
-
-    return make_response(jsonify({
-        'status' : 404,
-        'error' : 'Party not found.'
-    }))
+    if response[0] == 200:
+        return make_response(jsonify({
+            'status' : response[0],
+            'message' : response[1]
+        }), response[0])
+    else:
+        return make_response(jsonify({
+            'status' : response[0],
+            'error' : response[1]
+        }), response[0])
