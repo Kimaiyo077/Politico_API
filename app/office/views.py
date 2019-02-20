81cff45dd261b92debe671cdef9b98478f53e439
Isaac Kimaiyo <biwot777@gmail.com>
2019-02-21 01:41:48 +0300

    [Finishes #164125003] refactors code to use fewer lines and more methods

--- app/office/views.py
+++ app/office/views.py
@@ -5,15 +5,12 @@ from app.office import office
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
-@office.route('/offices', methods=['GET'])
-@jwt_required
-def get_all_offices():
-    response = OfficeModel.get_all_offices()
-    
-    if response[0] == 200:
+def response_message(response):
+    if response[0] == 201 or response[0] == 200:
         return make_response(jsonify({
             'status' : response[0],
-            'data' : response[1]
+            'token' : response[1],
+            'User' : response[2]
         }), response[0])
     else:
         return make_response(jsonify({
@@ -21,6 +18,14 @@ def get_all_offices():
             'error' : response[1]
         }), response[0])
 
+
+@office.route('/offices', methods=['GET'])
+@jwt_required
+def get_all_offices():
+    response = OfficeModel.get_all_offices()
+    message = response_message(response)
+
+    return message
 
 
 @office.route('/offices', methods=['POST'])
@@ -30,32 +35,16 @@ def add_office():
     current_user = get_jwt_identity()
     response = OfficeModel.create_office(data, current_user)
 
-    if response[0] == 201:
-        return make_response(jsonify({
-            'status' : response[0],
-            'data' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
 
 @office.route('/offices/<office_id>', methods=['GET'])
 @jwt_required
 def get_a_specific_office(office_id):
     response = OfficeModel.get_specific_office(int(office_id))
 
-    if response[0] == 200:
-        return make_response(jsonify({
-            'status' : response[0],
-            'data' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
   
 @office.route('/offices/<office_id>', methods=['PATCH'])
 @jwt_required
@@ -64,16 +53,8 @@ def edit_a_specific_office(office_id):
     data = request.get_json()
     response = OfficeModel.edit_specific_office(int(office_id), data, current_user)
 
-    if response[0] == 200:
-        return make_response(jsonify({
-            'status' : response[0],
-            'data' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
 
 @office.route('/offices/<office_id>', methods=['DELETE'])
 @jwt_required
@@ -81,16 +62,8 @@ def delete_a_office(office_id):
     current_user = get_jwt_identity()
     response = OfficeModel.delete_specific_office(int(office_id), current_user)
 
-    if response[0] == 200:
-        return make_response(jsonify({
-            'status' : response[0],
-            'message' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
 
 @office.route('/offices/<office_id>/register', methods=['POST'])
 @jwt_required
@@ -99,30 +72,14 @@ def add_candidate(office_id):
     data = request.get_json()
     response = OfficeModel.register_candidate(office_id, data, current_user)
 
-    if response[0] == 201:
-        return make_response(jsonify({
-            'status' : response[0],
-            'message' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
 
 @office.route('/offices/<office_id>/results', methods=['GET'])
 @jwt_required
 def print_results(office_id):
     response = OfficeModel.count_votes(office_id)
 
-    if response[0] == 200:
-        return make_response(jsonify({
-            'status' : response[0],
-            'message' : response[1]
-        }), response[0])
-    else:
-        return make_response(jsonify({
-            'status' : response[0],
-            'error' : response[1]
-        }), response[0])
+    message = response_message(response)
+    return message
 
